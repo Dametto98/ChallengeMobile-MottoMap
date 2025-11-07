4abe6944b358c28cefff058adf98562cffb3dcfc
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-11-06 21:05:15 -0300

    Internacionalização pelo idioma do dispositivo adicionado

--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,29 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import * as Localization from 'expo-localization'; // 1. IMPORTE A BIBLIOTECA
 
 import pt from './locales/pt.json';
-import es from './locales/es.json'; 
+import es from './locales/es.json';
+
+const locales = Localization.getLocales();
+let deviceLanguage = 'pt'; // Padrão, caso algo falhe
+
+if (locales && locales.length > 0) {
+  deviceLanguage = locales[0].languageCode;
+}
 
 i18n
   .use(initReactI18next) 
   .init({
     compatibilityJSON: 'v3', 
-    lng: 'es', 
-    fallbackLng: 'es', 
+    
+    lng: deviceLanguage,
+
+    fallbackLng: 'pt', // Idioma para o qual ele deve reverter se o idioma do celular não for 'pt' nem 'es'
 
     resources: {
       pt: pt,
-      es: es 
+      es: es
     },
 
     interpolation: {
