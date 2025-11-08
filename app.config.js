2c0c873661d43a3823cf50bfcb894bb1fca17384
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-11-08 01:04:45 -0300

    Correção eas projectId

--- app.config.js
+++ app.config.js
@@ -1,12 +1,12 @@
-const { execSync } = require('child_process');
-const myPackage = require('./package.json');
+const { execSync } = require("child_process");
+const myPackage = require("./package.json");
 
 let commitHash;
 try {
-  commitHash = execSync('git rev-parse --short HEAD').toString().trim();
+  commitHash = execSync("git rev-parse --short HEAD").toString().trim();
 } catch (e) {
   console.warn('Não foi possível pegar o hash do commit, usando "unknown"');
-  commitHash = 'unknown';
+  commitHash = "unknown";
 }
 
 const appVersion = myPackage.version;
@@ -15,39 +15,36 @@ module.exports = {
   expo: {
     name: "MotoMap",
     slug: "MottoMap",
-    version: appVersion, 
+    version: appVersion,
     orientation: "portrait",
     icon: "./assets/images/mottomap-logo.png",
-    
-    eas: {
-      "projectId": "389af45f-5ad5-4414-87e3-4c20e77a443d"
-    },
-    
+
     extra: {
       commitHash: commitHash,
       appVersion: appVersion,
+      eas: {
+        projectId: "389af45f-5ad5-4414-87e3-4c20e77a443d",
+      },
     },
-    
+
     splash: {
       image: "./assets/images/mottomap-logo.png",
       resizeMode: "contain",
-      backgroundColor: "#121212"
+      backgroundColor: "#121212",
     },
-    assetBundlePatterns: [
-      "**/*"
-    ],
+    assetBundlePatterns: ["**/*"],
     ios: {
-      "supportsTablet": true
+      supportsTablet: true,
     },
     android: {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/mottomap-logo.png",
-        "backgroundColor": "#121212"
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/mottomap-logo.png",
+        backgroundColor: "#121212",
       },
-      "package": "com.mottomap.app" 
+      package: "com.mottomap.app",
     },
     web: {
-      "favicon": "./assets/images/mottomap-logo.png"
-    }
-  }
+      favicon: "./assets/images/mottomap-logo.png",
+    },
+  },
 };
