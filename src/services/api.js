2faddad780fefa255ebd16868e09a7d64593405b
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-11-08 00:23:06 -0300

    Trocando a url de conexão para a url do render que está rodando a API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API_JAVA_URL = "http://10.0.2.2:8080"; //conexão utilizando o emulador tem que usar esse IP
+const API_JAVA_URL = "https://mottomap-api.onrender.com"; //Conexão com a API no render
 
 const apiJava = axios.create({
   baseURL: API_JAVA_URL,
